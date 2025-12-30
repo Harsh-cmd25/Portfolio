bf7586aeb6a6e5cc3ea7c05f312babf3948e4280
Harsh-cmd25 <doijodviju@gmail.com>
2025-12-31 01:41:52 +0530

    redy for deployement next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+export default [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    languageOptions: {
+      parser: "@typescript-eslint/parser", // ✅ parser को string के रूप में define करें
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+  },
+];
 
-export default eslintConfig;
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+// export default eslintConfig;
